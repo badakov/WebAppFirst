302419718a89c836316129a36e4efc656a8845a4
awscore <artur.badakov@gmail.com>
2024-11-27 23:28:26 +0300

    Добавлены страницы html

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
-
+from blog import views
 
 urlpatterns = [
     path('', views.index),
+    path('about/', views.about),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    header = 'Данные пользователя'
-    langs = ['Python', 'Java', 'C#']
-    user = {'name': 'Tom', 'age': 23}
-    address = ('Абрикосовая', 23, 45)
+    return render(request, 'base.html')
 
-    data = {'header': header, 'langs': langs, 'user': user, 'address': address}
-    return render(request, 'blog/index.html', context=data)
+def about(request):
+    return render(request, 'blog/about.html')
